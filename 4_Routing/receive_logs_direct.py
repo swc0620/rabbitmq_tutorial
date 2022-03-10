5373b3881fd00087d32d5a3c4c139638b5eea1f6
swc0620 <pinetreewave@naver.com>
2022-03-10 19:51:28 +0900

    4 Routing

---
+++ 4_Routing/receive_logs_direct.py
@@ -0,0 +1,26 @@
+import sys, pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+severities = sys.argv[1:]
+if not severities:
+    sys.stderr.write(f'Usage: {sys.argv[0]} [info] [warning] [error]\n')
+    sys.exit(1)
+
+for severity in severities:
+    channel.queue_bind(exchange='direct_logs', queue=queue_name, routing_key=severity)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(f' [x] {method.routing_key}:{body}')
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
