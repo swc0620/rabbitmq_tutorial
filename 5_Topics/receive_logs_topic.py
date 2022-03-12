0618d24992e8d98076fa74bd6381e9add27f3095
swc0620 <pinetreewave@naver.com>
2022-03-12 22:28:50 +0900

    5 Topics

---
+++ 5_Topics/receive_logs_topic.py
@@ -0,0 +1,27 @@
+import queue
+import pika, sys
+
+connection =pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write(f'Usage: {sys.argv[0]} [binding_key]...\n')
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(exchange='topic_logs', queue=queue_name, routing_key=binding_key)
+
+print(f' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(f' [x] {method.routing_key}:{body}')
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
